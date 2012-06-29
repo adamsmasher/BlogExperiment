879abf7ed8437d11e800066fc44b67ab61417634
Adam Smith <adam@tablethotels.com>
2012-06-29 15:08:59 -0400

    get DB info from properties
    Signed-off-by: Adam Smith <adam@tablethotels.com>

--- DB.scala
+++ DB.scala
@@ -4,8 +4,11 @@ import java.sql.Connection;
 object DB {
   def connect() : Connection = {
     Class.forName("org.postgresql.Driver");
+    val port = System.getProperty("BLOGDB_PORT");
+    val username = System.getProperty("BLOGDB_USERNAME");
+    val password = System.getProperty("BLOGDB_PASSWORD");
     return DriverManager.getConnection(
-      "jbdc:postgresql://localhost:PORT/blog",
-      "USERNAME", "PASSWORD");
+      "jbdc:postgresql://localhost:"+port+"/blog",
+      username, password);
   }
 }
