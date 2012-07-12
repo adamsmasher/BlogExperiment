88a2881276587ed172da6eb32527bddc9cb20b5f
Adam Smith <adam@tablethotels.com>
2012-07-12 19:41:54 -0400

    pull Index out into its own module
    Signed-off-by: Adam Smith <adam@tablethotels.com>

--- BlogApp.scala
+++ BlogApp.scala
@@ -1,4 +1,3 @@
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import org.postgresql.util.PSQLState;
 import org.stringtemplate.v4._;
@@ -8,61 +7,17 @@ object BlogApp extends FCGIHandler {
   val db = DB.connect();
   val findPostSQL = db.prepareStatement(
     "SELECT * FROM post WHERE id = ?");
-  val findPostListSQL = db.prepareStatement(
-    "SELECT * FROM post ORDER BY timestamp DESC LIMIT 10 OFFSET ?"
-  );
-  val postCountSQL = db.prepareStatement(
-    "SELECT count(*) count FROM post");
+
+  val indexPage = new IndexPage(db);
+
   def main(args: Array[String]) = {
     for(req <- get_request()) {
       val response = req dispatch Map(
-        "/(index)?" -> index,
+        "/(index)?" -> indexPage.index,
         "/post" -> post
       )
       System.out.println(response);
     }
-  }
-
-  def index(req: FCGIRequest): HTTPResponse = {
-    val pageNum = (for {
-      pageNumStr <- req.fields.get("page")
-      pageNum <- IntUtil.toInt(pageNumStr)
-    } yield pageNum) match {
-      case Some(pageNum) => pageNum
-      case None => 0
-    }
-
-    val template = new STGroupDir("templates", '$', '$').getInstanceOf("index");
-
-    template.add("next_page", nextPage(pageNum).orNull);
-    template.add("previous_page", previousPage(pageNum).orNull);
-
-    val results = findPostList(pageNum);
-    while(results.next()) {
-      template.add("posts", Post.fromRow(results));
-    }
-
-    return new HTTPResponse(
-      HTMLMIME,
-      Array(),
-      template.render());
-  }
-
-  def nextPage(currentPage: Int): Option[Int] = {
-    val postCount = getPostCount();
-    // return the next page number only if there are more pages
-    // to display
-    if(currentPage * 10 + 10 > postCount)
-      return None;
-    else
-      return Some(currentPage + 1);
-  }
-
-  def previousPage(currentPage: Int): Option[Int] = {
-    if(currentPage > 0)
-      return Some(currentPage - 1);
-    else
-      return None;
   }
 
   def post(req: FCGIRequest): HTTPResponse = {
@@ -99,17 +54,6 @@ object BlogApp extends FCGIHandler {
 	}
       }
     }
-  }
-
-  def findPostList(page:Int) : ResultSet = {
-    findPostListSQL.setInt(1, page * 10);
-    return findPostListSQL.executeQuery();
-  }
-
-  def getPostCount() : Int = {
-    val row = postCountSQL.executeQuery();
-    row.next();
-    return row.getInt("count");
   }
 
   def postNotFound() : HTTPResponse = {
---
+++ IndexPage.scala
@@ -0,0 +1,64 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import org.stringtemplate.v4.STGroupDir;
+
+class IndexPage(db: Connection) {
+  val findPostListSQL = db.prepareStatement(
+    "SELECT * FROM post ORDER BY timestamp DESC LIMIT 10 OFFSET ?"
+  );
+  val postCountSQL = db.prepareStatement(
+    "SELECT count(*) count FROM post");
+
+  def index(req: FCGIRequest): HTTPResponse = {
+    val pageNum = (for {
+      pageNumStr <- req.fields.get("page")
+      pageNum <- IntUtil.toInt(pageNumStr)
+    } yield pageNum) match {
+      case Some(pageNum) => pageNum
+      case None => 0
+    }
+
+    val template = new STGroupDir("templates", '$', '$').getInstanceOf("index");
+
+    template.add("next_page", nextPage(pageNum).orNull);
+    template.add("previous_page", previousPage(pageNum).orNull);
+
+    val results = findPostList(pageNum);
+    while(results.next()) {
+      template.add("posts", Post.fromRow(results));
+    }
+
+    return new HTTPResponse(
+      HTMLMIME,
+      Array(),
+      template.render());
+  }
+
+  def nextPage(currentPage: Int): Option[Int] = {
+    val postCount = getPostCount();
+    // return the next page number only if there are more pages
+    // to display
+    if(currentPage * 10 + 10 > postCount)
+      return None;
+    else
+      return Some(currentPage + 1);
+  }
+
+  def previousPage(currentPage: Int): Option[Int] = {
+    if(currentPage > 0)
+      return Some(currentPage - 1);
+    else
+      return None;
+  }
+
+  def findPostList(page:Int) : ResultSet = {
+    findPostListSQL.setInt(1, page * 10);
+    return findPostListSQL.executeQuery();
+  }
+
+  def getPostCount() : Int = {
+    val row = postCountSQL.executeQuery();
+    row.next();
+    return row.getInt("count");
+  }
+}
