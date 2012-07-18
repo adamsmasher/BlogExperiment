d96368b7561954d8649992e1b15b37ddec040fe3
Adam Smith <adam@tablethotels.com>
2012-07-18 16:19:52 -0400

    Add permalinks to blog posts
    Signed-off-by: Adam Smith <adam@tablethotels.com>

--- Post.scala
+++ Post.scala
@@ -5,13 +5,15 @@ import scala.reflect.BeanProperty;
 object Post {
   def fromRow(row:ResultSet) : Post = {
     return new Post(
+      row.getInt("id"),
       row.getString("title"),
       row.getString("contents"),
       row.getTimestamp("timestamp"));
   }
 }
 
-class Post(@BeanProperty val title: String,
+class Post(@BeanProperty val id: Int,
+           @BeanProperty val title: String,
 	   @BeanProperty val contents: String,
 	   @BeanProperty val timestamp: Timestamp)
 {
