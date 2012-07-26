be8323cc81f7c7295b290afd4232d4809af075c8
Adam Smith <adam@tablethotels.com>
2012-07-26 17:03:35 -0400

    add a page to confirm post deletion
    Signed-off-by: Adam Smith <adam@tablethotels.com>

--- BlogApp.scala
+++ BlogApp.scala
@@ -8,6 +8,7 @@ object BlogApp extends FCGIHandler {
   val postPage = new PostPage(db);
   val postCommentAction = new PostCommentAction(db);
   val adminPage = new AdminPage(db);
+  val deletePostPage = new DeletePostPage(db);
 
   def main(args: Array[String]) = {
     for(req <- get_request()) {
@@ -15,7 +16,8 @@ object BlogApp extends FCGIHandler {
         "/(index)?" -> indexPage.index,
         "/post" -> postPage.post,
         "/post_comment" -> postCommentAction.postComment,
-        "/admin" -> adminPage.admin
+        "/admin" -> adminPage.admin,
+        "/admin/delete_post" -> deletePostPage.confirm
       )
       System.out.println(response);
     }
---
+++ DeletePostPage.scala
@@ -0,0 +1,67 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import org.postgresql.util.PSQLState;
+import scala.reflect.BeanProperty;
+
+class DeletePostPage(db: Connection) {
+  val getPostInfoSQL = db.prepareStatement(
+    "SELECT title FROM post WHERE id = ?");
+  
+  def confirm(req: FCGIRequest): HTTPResponse = {
+    val postInfo = for {
+      postIdStr <- req.fields.get("id")
+      postId <- IntUtil.toInt(postIdStr)
+      postInfo <- getPostInfo(postId)
+    } yield postInfo;
+    return postInfo match {
+      case Some(postInfo) => confirmResponse(postInfo)
+      case None           => badRequest()
+    };
+  }
+
+  def badRequest() : HTTPResponse = {
+    return new HTTPResponse(
+      HTMLMIME,
+      Array(),
+      "<HTML>Bad request.</HTML>");
+  }
+
+  def confirmResponse(postInfo:PostInfo) : HTTPResponse = {
+    val template = Templates.get("delete_post");
+    
+    template.add("id", postInfo.id);
+    template.add("title", postInfo.title);
+
+    return new HTTPResponse(
+      HTMLMIME,
+      Array(),
+      template.render());
+  }
+
+  def getPostInfo(postId:Int) : Option[PostInfo] = {
+    getPostInfoSQL.setInt(1, postId);
+    try {
+      val row = getPostInfoSQL.executeQuery();
+      row.next();
+      return Some(new PostInfo(postId, row.getString("title")));
+    } catch {
+      case e:SQLException => {
+        // The cursor will be in an invalid state after next()
+        // if the result set is empty
+        if(e.getSQLState() equals PSQLState.INVALID_CURSOR_STATE.getState())
+        {
+          return None;
+        }
+        else {
+          BlogApp.log.println(e.getSQLState());
+          BlogApp.log.println(e);
+          throw e;
+        }
+      }
+    }
+  }
+}
+
+
+class PostInfo(@BeanProperty val id:Int, @BeanProperty val title:String) {
+}
