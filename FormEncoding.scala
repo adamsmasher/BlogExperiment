c1da9951cd3c51d7f95548a353d1336c915b90d9
Adam Smith <adam@tablethotels.com>
2012-07-26 11:16:08 -0400

    Form decode stuff
    Signed-off-by: Adam Smith <adam@tablethotels.com>

--- FormEncoding.scala
+++ FormEncoding.scala
@@ -1,9 +1,11 @@
+import java.net.URLDecoder;
+
 object FormEncoding {
   def parseFormEncoded(qs:String) : Map[String, String] = {
     return Map {
       qs split '&' map {
         (s:String) => s.splitAt(s indexOf '=') match {
-          case (k, ev) => (k, ev drop 1)
+          case (k, ev) => (k, URLDecoder.decode(ev drop 1))
         }
       } : _*
     }
