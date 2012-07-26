6549a221574a88be3dc5e1b48b982590a6655894
Adam Smith <adam@tablethotels.com>
2012-07-26 10:36:46 -0400

    fix indentation
    Signed-off-by: Adam Smith <adam@tablethotels.com>

--- URL.scala
+++ URL.scala
@@ -2,9 +2,9 @@ object QueryString {
   def parseQueryString(qs:String) : Map[String, String] = {
     return Map {
       qs split '&' map {
-	(s:String) => s.splitAt(s indexOf '=') match {
-	  case (k, ev) => (k, ev drop 1)
-	}
+        (s:String) => s.splitAt(s indexOf '=') match {
+          case (k, ev) => (k, ev drop 1)
+        }
       } : _*
     }
   }
