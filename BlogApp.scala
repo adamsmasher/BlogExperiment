9efc1d6c7f426ff4379cfadf9e0331b0e2550a69
Adam Smith <adam@tablethotels.com>
2012-07-26 18:16:42 -0400

    add ability to delete posts
    Signed-off-by: Adam Smith <adam@tablethotels.com>

--- BlogApp.scala
+++ BlogApp.scala
@@ -7,6 +7,7 @@ object BlogApp extends FCGIHandler {
   val postCommentAction = new PostCommentAction(db);
   val adminPage = new AdminPage(db);
   val deletePostPage = new DeletePostPage(db);
+  val deletePostAction = new DeletePostAction(db);
 
   def main(args: Array[String]) = {
     for(req <- get_request()) {
@@ -15,7 +16,8 @@ object BlogApp extends FCGIHandler {
         "/post" -> postPage.post,
         "/post_comment" -> postCommentAction.postComment,
         "/admin" -> adminPage.admin,
-        "/admin/delete_post" -> deletePostPage.confirm
+        "/admin/delete_post" -> deletePostPage.confirm,
+        "/admin/do_delete_post" -> deletePostAction.deletePost
       )
       System.out.println(response);
     }
---
+++ DeletePostAction.scala
@@ -0,0 +1,45 @@
+import java.sql.Connection;
+
+class DeletePostAction(db: Connection) {
+  def deletePost(req: FCGIRequest): HTTPResponse = {
+    val postId = for { 
+      postIdStr <- req.fields.get("id")
+      postId <- IntUtil.toInt(postIdStr)
+    } yield postId;
+
+    return postId match {
+      case Some(postId) => doDeletePost(postId);
+      case None         => badRequest();
+    };
+  }
+
+  def badRequest() : HTTPResponse = {
+    return new HTTPResponse(
+      HTMLMIME,
+      Array(),
+      "<HTML>Bad request.</HTML>");
+  }
+
+  def redirectToAdmin() : HTTPResponse = {
+    return new HTTPResponse(
+      HTMLMIME,
+      Array(),
+      "<HTML>Redirect.</HTML>");
+  }
+
+  def doDeletePost(postId: Int) : HTTPResponse =
+  {
+    db.setAutoCommit(false);
+    val deletePostSQL = db.prepareStatement("DELETE FROM post WHERE id = ?");
+    val deleteCommentsSQL = db.prepareStatement(
+      "DELETE FROM comment WHERE post_id = ?");
+    deletePostSQL.setInt(1, postId);
+    deleteCommentsSQL.setInt(1, postId);
+    deleteCommentsSQL.executeUpdate();
+    deletePostSQL.executeUpdate();
+    db.commit();
+    db.setAutoCommit(true);
+
+    return redirectToAdmin();
+  }
+}
